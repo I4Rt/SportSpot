8d5ec11a5435e905badb68f9b24d64bcb3235d42
Markov Vladimir <84292343+I4Rt@users.noreply.github.com>
2023-07-05 16:22:53 +0700

    Refresh for the tokens
    Refresh are done

--- UserBackend/config.py
+++ UserBackend/config.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS, cross_origin
+from datetime import timedelta
 
 app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path = '')
 app.config['TIME'] = time()
@@ -11,6 +12,9 @@ app.config['SIZE'] = 1000
 
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
 
 bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:qwerty@localhost:5432/sport_object_main'
