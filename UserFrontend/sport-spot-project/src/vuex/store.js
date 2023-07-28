6e13cc7661cac93871f7e6d09db7b5f951486ef9
Andrey Ivanchin <92378488+EpubOW@users.noreply.github.com>
2023-07-28 15:43:27 +0700

    28.07
    - form for adding rooms and sectors
    - local connection with cameras, rooms and sectors
    -

--- UserFrontend/sport-spot-project/src/vuex/store.js
+++ UserFrontend/sport-spot-project/src/vuex/store.js
@@ -5,22 +5,68 @@ import { createStore } from 'vuex'
 export default createStore({
     state () {
         return {
-            cameras: []
+            cameraCount: 0,
+            roomCount: 0,
+            cameras: [],
+            rooms: [],
+            sectorTypes: ['ПОЛ', 'ЗЕРКАЛО']
         }
     },
     mutations: {
         setCameras (state, camera) {
             state.cameras.push(camera)
+        },
+        setRooms (state, room) {
+            state.rooms.push(room)
+        },
+        setCameraCount (state, cameraCount) {
+            state.cameraCount = cameraCount
+        },
+        setRoomCount (state, roomCount) {
+            state.roomCount = roomCount
         }
     },
     actions: {
-        addCamera({commit}, camera) {
-            commit('setCameras', camera)
+        addCamera({commit}, newCamera) {
+            commit('setCameras', newCamera)
+        },
+        addRoom({commit}, newRoom) {
+            commit('setRooms', newRoom)
+        },
+        addCameraCount({commit}, cameraCount) {
+            commit('setCameraCount', cameraCount)
+        },
+        addRoomCount({commit}, roomCount) {
+            commit('setRoomCount', roomCount)
         }
     },
     getters: {
         getCameras(state) {
             return state.cameras
+        },
+        getCameraByID: (state) => (id) => {
+            return state.cameras.find(camera => camera.id === id)
+        },
+        getCamerasByRoomID: (state) => (id) => {
+          return state.cameras.filter(camera => camera.roomID === id)
+        },
+        getCamerasWithoutRoomID (state) {
+            return state.cameras.filter(camera => camera.roomID === -1)
+        },
+        getRooms(state) {
+            return state.rooms
+        },
+        getRoomByID: (state) => (id) => {
+            return state.rooms.find(room => room.id === id)
+        },
+        getSectorTypes(state) {
+            return state.sectorTypes
+        },
+        getCameraCount(state) {
+            return state.cameraCount
+        },
+        getRoomCount(state) {
+            return state.roomCount
         }
     }
 })
