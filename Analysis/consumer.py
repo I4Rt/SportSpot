4c70332f0f29467f1d389473294304ce313b95c0
Anton Khnykin <antonkhnykin@gmail.com>
2023-10-03 21:14:28 +0700

    Add files via upload

--- Analysis/consumer.py
+++ Analysis/consumer.py
@@ -25,11 +25,11 @@ def checkIfInside(border, target):
     """Checking if point in polygon or not."""
     return Polygon(border).contains(Point(target[0], target[1]))
 
-def recognition(sectors, taskID):
+def recognition(sectors):
     """Recognition image and send the counter."""
     image = cv2.imread("queue/image_receive.jpg")
 
-    results = model.predict(source=image, imgsz=1920, conf=0.25, classes=[0])
+    results = model.predict(source=image, imgsz=1920, conf=0.3, classes=[0])
     decImg_h, decImg_w = image.shape[:2]
 
     counter = 0
@@ -52,10 +52,7 @@ def recognition(sectors, taskID):
             if checkIfInside(border, (box.xyxy[0][0].item(), box.xyxy[0][3].item())) and \
                     checkIfInside(border, (box.xyxy[0][2].item(), box.xyxy[0][3].item())):
                 counter += 1
-
-    producer.sendMessage({"taskID":  taskID, "counter": counter, "datetime": str(datetime.datetime.now())})
-
-    return
+    return counter
 
 
 class KafkaConsumerPlus:
@@ -94,15 +91,28 @@ if __name__ == "__main__":
     print("starting the consumer ImageReceiver")
 
     for msg in consumer.consumer:
-#        print(msg)
+        aggregationMode = 1
+        taskID = 0
+        res_counter = 0
         for key_main in json.loads(msg.value):
-#            print("key_main", key_main)
+            if key_main == "taskID":
+                taskID = json.loads(msg.value)["taskID"]
+            if key_main == "aggregationMode":
+                aggregationMode = json.loads(msg.value)["aggregationMode"]
             if key_main == "data":
-#                print('data')
+                list_counter = []
                 for key_data in json.loads(msg.value)["data"]:
                     readImgBytes = base64.b64decode(key_data["img"])
                     npImg = frombuffer(readImgBytes, 'u1')
                     decImg = cv2.imdecode(npImg, 1)
                     cv2.imwrite("queue/image_receive.jpg", decImg)
-                    recognition(key_data["sectors"], json.loads(msg.value)["taskID"])
-#                    print("Sending")
+                    list_counter.append(recognition(key_data["sectors"]))
+                # Sum the people, aggregationMode=1
+                if aggregationMode == 1:
+                    res_counter = sum(list_counter)
+                # Find maximum, aggregationMode=2
+                else:
+                    res_counter = max(list_counter)
+
+        producer.sendMessage({"taskID":  taskID, "counter": res_counter, "aggregationMode": aggregationMode,
+                              "datetime": str(datetime.datetime.now())})
